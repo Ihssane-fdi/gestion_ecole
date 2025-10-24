bd947e5b02ab3e3aad6e0d6122cfc724a4ef2f81
Zakariae Taibi <zakariaetaibi103gmail.com>
2025-10-24 11:18:53 +0100

    les pages des formulaire

---
+++ connexion.php
@@ -0,0 +1,10 @@
+<?php
+
+$serveur = "localhost";
+$utilisateur = "root";
+$motdepasse = ""; 
+$basededonnees = "gestion_ecole";
+
+$conn = new mysqli($serveur, $utilisateur, $motdepasse, $basededonnees);
+
+?>
